93e6c9561f716f6b82d01884a7fb9656c2a7fdfb
Eric Evans <eevans@racklabs.com>
2008-05-19 15:11:16 -0500

    updated version information

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 setup(
     name='freerange',
-    version='0.1',
+    version='0.9.0',
     description='CloudFS client library for python',
     author='Racklabs',
     url='https://www.mosso.com/cloudfs',
