f779fc311a68dc3c4058d41f4a5bed8f8e362c0c
Jay Farrimond <jay.farrimond@rackspace.com>
2009-03-18 02:04:41 -0500

    python-cloudfiles v1.3.0

--- cloudfiles/consts.py
+++ cloudfiles/consts.py
@@ -1,6 +1,6 @@
 """ See COPYING for license information. """
 
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 user_agent = "python-cloudfiles/%s" % __version__
 default_authurl = 'https://api.mosso.com/auth'
 default_cdn_ttl = 86400
